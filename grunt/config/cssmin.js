9200680031415ac04b6d17a268429e091b55b289
Onatolich <onatolich@gmail.com>
2016-03-31 11:17:38 +0300

    Add css minifier

---
+++ grunt/config/cssmin.js
@@ -0,0 +1,10 @@
+module.exports = {
+    options: {
+        sourceMap: true
+    },
+    target: {
+        files: {
+            'theme/css/style.min.css': 'theme/css/style.css'
+        }
+    }
+};
--- grunt/tasks/init.js
+++ grunt/tasks/init.js
@@ -1,3 +1,3 @@
 module.exports = function(grunt) {
-    grunt.registerTask('init', ['bake:dev', 'sass']);
+    grunt.registerTask('init', ['bake:dev', 'sass', 'cssmin']);
 };
