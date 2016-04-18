28bda7f0ab55a3487c8c6251f89ee957b928e049
Onatolich <onatolich@gmail.com>
2016-04-18 11:24:43 +0300

    Add handlebars

--- src/require.config.js
+++ src/require.config.js
@@ -2,11 +2,17 @@ requirejs.config({
     baseUrl: '/src',
 
     paths: {
+        // Plugins
+        hbs: 'bower_components/require-handlebars-plugin/hbs',
+
+        // Generic
         node_modules: '../node_modules',
         jquery: 'bower_components/jquery/dist/jquery.min',
         underscore: 'bower_components/underscore/underscore-min',
         backbone: 'bower_components/backbone/backbone-min',
+        handlebars: 'bower_components/handlebars/handlebars.min',
 
+        // Paths
         Util: 'common/util/Util'
     },
 
@@ -20,6 +26,9 @@ requirejs.config({
         backbone: {
             deps: ['jquery', 'underscore'],
             exports: 'Backbone'
+        },
+        handlebars: {
+            exports: 'Handlebars'
         }
     }
 });
