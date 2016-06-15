837278dadd8b2994ab31eb2dfd20234037e7a645
Onatolich <onatolich@gmail.com>
2016-06-15 15:09:27 +0300

    Update grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
         loadGruntTasks: {
             pattern: 'grunt-*',
             config: require('./package.json'),
-            scope: 'dependencies'
+            scope: 'devDependencies'
         }
     });
 
